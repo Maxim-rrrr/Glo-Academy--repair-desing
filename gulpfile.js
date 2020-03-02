5741d6c73d3384a087b1f56d51970b39e23dd980
Maxim <kewin.rrrr@gmail.com>
2020-03-02 08:53:32 +0300

    create gulp.task('cssmin') & create style.min.css

--- gulpfile.js
+++ gulpfile.js
@@ -10,3 +10,14 @@ gulp.task('browser-sync', function() {
     });
     gulp.watch("./*.html").on('change', browserSync.reload);
 });
+
+// gulp-cssmin
+var cssmin = require('gulp-cssmin');
+var rename = require('gulp-rename');
+
+gulp.task('cssmin', function() {
+    gulp.src('src/**/*.css')
+        .pipe(cssmin())
+        .pipe(rename({ suffix: '.min' }))
+        .pipe(gulp.dest('dist'));
+});
