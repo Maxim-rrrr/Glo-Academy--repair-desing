6644911516f14ecb7e32da682cd99ba4eaea69bf
Maxim <kewin.rrrr@gmail.com>
2020-03-20 04:19:43 +0300

    feat: added section customers

--- send.php
+++ send.php
@@ -15,21 +15,21 @@ $mail = new PHPMailer\PHPMailer\PHPMailer();
 
 try {
     //Server settings
-    $mail->SMTPDebug = 0;                      // Enable verbose debug output
+    $mail->SMTPDebug = 0;                                       // Enable verbose debug output
     $mail->isSMTP();                                            // Send using SMTP
-    $mail->Host       = 'smtp.gmail.com';                    // Set the SMTP server to send through
+    $mail->Host       = 'smtp.gmail.com';                       // Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
-    $mail->Username   = 'aspirine20017270@gmail.com';                     // SMTP username
-    $mail->Password   = 'maxi7270';                               // SMTP password
-    $mail->SMTPSecure = 'ssl';         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` also accepted
+    $mail->Username   = 'aspirine20017270@gmail.com';           // SMTP username
+    $mail->Password   = 'maxi7270';                             // SMTP password
+    $mail->SMTPSecure = 'ssl';                                  // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` also accepted
     $mail->Port       = 465;                                    // TCP port to connect to
 
     //Recipients
     $mail->setFrom('aspirine20017270@gmail.com', 'repair-desing');
-    $mail->addAddress('kewin.rrrr@gmail.com');     // Add a recipient
+    $mail->addAddress('kewin.rrrr@gmail.com');                  // Add a recipient
 
     // Content
-    $mail->isHTML(true);                                  // Set email format to HTML
+    $mail->isHTML(true);                                        // Set email format to HTML
     $mail->Subject = "New application repair-dasing";
     $mail->Body    = "Имя пользователя: ${userName}, его телефон: ${userPhone}. Его почта: ${userEmail}. Его вопрос: ${userQuestion}.";
 
