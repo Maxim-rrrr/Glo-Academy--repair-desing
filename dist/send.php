37b6af2f6fa11bf2cf0327999932528da5921875
Maxim <kewin.rrrr@gmail.com>
2020-03-23 00:47:51 +0300

    feat: dist

---
+++ dist/send.php
@@ -0,0 +1,44 @@
+<?php
+
+$userName = $_POST['userName'];
+$userEmail = $_POST['userEmail'];
+$userPhone = $_POST['userPhone'];
+$userQuestion = $_POST['userQuestion'];
+
+// Load Composer's autoloader
+require 'phpmailer/PHPMailer.php';
+require 'phpmailer/SMTP.php';
+require 'phpmailer/Exception.php';
+
+// Instantiation and passing `true` enables exceptions
+$mail = new PHPMailer\PHPMailer\PHPMailer();
+
+try {
+  //Server settings
+  $mail->SMTPDebug = 0;                                       // Enable verbose debug output
+  $mail->isSMTP();                                            // Send using SMTP
+  $mail->Host       = 'smtp.gmail.com';                       // Set the SMTP server to send through
+  $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
+  $mail->Username   = 'aspirine20017270@gmail.com';           // SMTP username
+  $mail->Password   = 'maxi7270';                             // SMTP password
+  $mail->SMTPSecure = 'ssl';                                  // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` also accepted
+  $mail->Port       = 465;                                    // TCP port to connect to
+
+  //Recipients
+  $mail->setFrom('aspirine20017270@gmail.com', 'repair-desing');
+  $mail->addAddress('kewin.rrrr@gmail.com');                  // Add a recipient
+
+  // Content
+  $mail->CharSet = "UTF-8";
+  $mail->isHTML(true);                                        // Set email format to HTML
+  $mail->Subject = "Новое сообщение repair-dasing";
+  $mail->Body    = "Имя пользователя: ${userName}, его телефон: ${userPhone}. Его почта: ${userEmail}. Его вопрос: ${userQuestion}.";
+
+  if ($mail->send()) {
+    echo "Ok";
+  } else {
+    echo "Письмо не отправлено, есть ошибка. Код ошибки: {$mail->ErrorInfo}";
+  }
+} catch (Exception $e) {
+  echo "Письмо не отправлено, есть ошибка. Код ошибки: {$mail->ErrorInfo}";
+}
